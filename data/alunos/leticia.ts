d119a1f8a31f8924bb42a57c3f668f5b0541afd9
leticia <leticiaferreiracostabarrias2@gmail.com>
2026-07-30 16:32:28 -0300

    adicionei os meus dados

---
+++ data/alunos/leticia.ts
@@ -0,0 +1,12 @@
+import type { Commit } from "../turma";
+
+export const commits: Commit[] = [
+  {
+    nome: "leticia",
+    usuario: "leticia",
+    mensagem: "feat: meu primeiro commit",
+    nota: "primeiro commit é sempre o mais difícil.",
+    data: "2026-07-30",
+  },
+];
+
