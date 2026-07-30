d848b024c777b717b242609fe880d8dd77b0a709
Marcos de Oliveira <mods0109@gmail.com>
2026-07-30 16:32:37 -0300

    adicionei os meu dados

---
+++ data/alunos/marcos.ts
@@ -0,0 +1,11 @@
+import type { Commit } from "../turma";
+
+export const commits: Commit[] = [
+  {
+    nome: "Marcos de Oliveira",
+    usuario: "marcos2134y",
+    mensagem: "feat: meu primeiro commit",
+    nota: "aulo do marcelo",
+    data: "2026-07-30",
+  },
+];
