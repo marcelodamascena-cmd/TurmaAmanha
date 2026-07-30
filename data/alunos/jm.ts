6203df299b023aa5ca0c06c60889c7ced7e065a8
Joao Matheus <joaovascofodaribamar@gmail.com>
2026-07-30 16:32:33 -0300

    adicionei meus dados

---
+++ data/alunos/jm.ts
@@ -0,0 +1,11 @@
+import type { Commit } from "../turma";
+
+export const commits: Commit[] = [
+  {
+    nome: "Joao Matheus",
+    usuario: "joaovascao",
+    mensagem: "feat: meu primeiro commit",
+    nota: "hiperfoco em x-men — primeiro commit é sempre o mais difícil.",
+    data: "2026-07-30",
+  },
+];
